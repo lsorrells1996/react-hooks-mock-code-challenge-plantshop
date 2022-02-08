b20dd34873932aafb708a5fb220009a04ce0228e
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-02-08 09:22:18 -0600

    more fun

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList({plants}) {
+function PlantList({plantsToDisplay}) {
   return (
-    <ul className="cards">{plants.map(plant => <PlantCard key={plant.id} plant={plant} /> )}</ul>
+    <ul className="cards">{plantsToDisplay.map(plant => <PlantCard key={plant.id} plant={plant} /> )}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -6,6 +6,7 @@ import Search from "./Search";
 function PlantPage() {
 
   const [plants, setPlants] = useState([])
+  const [searchItem, setSearchItem] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:6001/plants')
@@ -14,21 +15,25 @@ function PlantPage() {
 
   }, [])
 
-  const createPlant = (newPlant) => {
+  const createPlant = newPlant => {
     setPlants(oldPlants => {
       return [...oldPlants, newPlant]
     })
   }
 
   const handleFilter = e => {
-    return e.target.value
+    setSearchItem(e.target.value)
   }
+
+  const plantsToDisplay = plants.filter(plant => {
+    return plant.name.toLowerCase().includes(searchItem.toLowerCase())
+  })
 
   return (
 
     <main>
       <NewPlantForm createPlant={createPlant} />
-      <Search />
+      <Search handleFilter={handleFilter} plantsToDisplay={plantsToDisplay}/>
       <PlantList plants={plants} />
     </main>
   );
